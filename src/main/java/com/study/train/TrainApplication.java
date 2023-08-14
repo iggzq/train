7e6d218a00e04ab0088f95025a4851a649fba965
LiTz <2811328244@qq.com>
2023-08-14 15:29:29 +0800

    初始化

---
+++ src/main/java/com/study/train/TrainApplication.java
@@ -0,0 +1,14 @@
+package com.study.train;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainApplication {
+
+    public static void main(String[] args) {
+        System.out.println("hello world12345");
+        SpringApplication.run(TrainApplication.class, args);
+    }
+
+}
