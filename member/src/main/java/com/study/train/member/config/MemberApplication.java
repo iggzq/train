8a40dbe82579826337df47aa4e956597e1dc6bcd
LiTz <2811328244@qq.com>
2023-08-14 15:50:40 +0800

    8.14.1 新增member模块

--- member/src/main/java/com/study/train/member/config/MemberApplication.java
+++ member/src/main/java/com/study/train/member/config/MemberApplication.java
@@ -1,14 +1,16 @@
-package com.study.train;
+package com.study.train.member.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class TrainApplication {
+@ComponentScan("com.study")
+public class MemberApplication {
 
     public static void main(String[] args) {
         System.out.println("hello world12345");
-        SpringApplication.run(TrainApplication.class, args);
+        SpringApplication.run(MemberApplication.class, args);
     }
 
 }
