98b76efb72b7cccf364cbdfcf41e8a18d898c0a7
LiTz <2811328244@qq.com>
2023-08-16 10:07:49 +0800

    8.16.1 实现一个简单的注册接口

--- member/src/main/java/com/study/train/member/service/MemberService.java
+++ member/src/main/java/com/study/train/member/service/MemberService.java
@@ -1,8 +1,13 @@
 package com.study.train.member.service;
 
+import com.study.train.member.domain.Member;
+import com.study.train.member.domain.MemberExample;
 import com.study.train.member.mapper.MemberMapper;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
+import java.util.List;
 
 @Service
 public class MemberService {
@@ -12,6 +17,22 @@ public class MemberService {
 
     public int count(){
         return Math.toIntExact(memberMapper.countByExample(null));
+    }
+
+    public long register(String mobile){
+        MemberExample memberExample = new MemberExample();
+        memberExample.createCriteria().andMobileEqualTo(mobile);
+        List<Member> members = memberMapper.selectByExample(memberExample);
+
+        if(CollectionUtils.isEmpty(members)){
+            throw new RuntimeException("手机号已注册");
+        }
+
+        Member member = new Member();
+        member.setId(System.currentTimeMillis());
+        member.setMobile(mobile);
+        memberMapper.insert(member);
+        return member.getId();
     }
 
 }
