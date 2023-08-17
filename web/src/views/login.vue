d5260ceede2afb73cc4491410bb68aaacbba5fa9
LiTz <2811328244@qq.com>
2023-08-17 12:36:09 +0800

    8.17.2 修改login界面，登陆注册二合一，后端接口未对接

--- web/src/views/login.vue
+++ web/src/views/login.vue
@@ -1,57 +1,69 @@
 <template>
-  <a-form
-      :model="formState"
-      name="basic"
-      :label-col="{ span: 8 }"
-      :wrapper-col="{ span: 16 }"
-      autocomplete="off"
-      @finish="onFinish"
-      @finishFailed="onFinishFailed"
-  >
-    <a-form-item
-        label="Username"
-        name="username"
-        :rules="[{ required: true, message: 'Please input your username!' }]"
-    >
-      <a-input v-model:value="formState.username" />
-    </a-form-item>
+  <a-row class="login">
+    <a-col :span="8" :offset="8" class="login-main">
+      <h1 style="text-align: center">
+        <rocket-two-tone/>
+        12306售票系统
+      </h1>
+      <a-form
+          :model="loginForm"
+          name="basic"
+          autocomplete="off"
+          @finish="onFinish"
+          @finishFailed="onFinishFailed"
+      >
+        <a-form-item
+            label=""
+            name="mobile"
+            :rules="[{ required: true, message: '请输入手机号' }]"
+        >
+          <a-input v-model:value="loginForm.mobile"/>
+        </a-form-item>
 
-    <a-form-item
-        label="Password"
-        name="password"
-        :rules="[{ required: true, message: 'Please input your password!' }]"
-    >
-      <a-input-password v-model:value="formState.password" />
-    </a-form-item>
+        <a-form-item
+            label=""
+            name="code"
+            :rules="[{ required: true, message: '请输入验证码'}]"
 
-    <a-form-item name="remember" :wrapper-col="{ offset: 8, span: 16 }">
-      <a-checkbox v-model:checked="formState.remember">Remember me</a-checkbox>
-    </a-form-item>
+        >
+          <a-input v-model:value="loginForm.code">
+            <template #addonAfter>
+              <a @click="sendCode">获取验证码</a>
+            </template>
+          </a-input>
+        </a-form-item>
 
-    <a-form-item :wrapper-col="{ offset: 8, span: 16 }">
-      <a-button type="primary" html-type="submit">Submit</a-button>
-    </a-form-item>
-  </a-form>
+
+        <a-form-item>
+          <a-button type="primary" block html-type="submit">提交</a-button>
+        </a-form-item>
+      </a-form>
+    </a-col>
+  </a-row>
+
+
 </template>
 
 <script>
-import { defineComponent, reactive } from 'vue';
+import {defineComponent, reactive} from 'vue';
+import {left} from "core-js/internals/array-reduce";
+
 export default defineComponent({
+  name: "login-view",
+  methods: {left},
   setup() {
-    const formState = reactive({
-      username: '',
-      password: '',
-      remember: true,
+    const loginForm = reactive({
+      mobile: '18212341234',
+      code: '',
     });
     const onFinish = values => {
       console.log('Success:', values);
     };
-
     const onFinishFailed = errorInfo => {
       console.log('Failed:', errorInfo);
     };
     return {
-      formState,
+      loginForm,
       onFinish,
       onFinishFailed,
     };
@@ -59,6 +71,18 @@ export default defineComponent({
 });
 </script>
 
-<style>
+<style scoped>
+.login-main h1 {
+  font-size: 25px;
+  font-weight: bold;
+}
+
+.login-main {
+  margin-top: 100px;
+  padding: 30px 30px 20px;
+  border: 2px solid grey;
+  border-radius: 10px;
+  background-color: #fcfcfc;
+}
 
 </style>
