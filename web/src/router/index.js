53631560e93e059970b80a8184a432cbd37d177f
LiTz <2811328244@qq.com>
2023-08-29 11:12:10 +0800

    8.29.5 增加路由登陆拦截，访问所有控台界面都需要登陆

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,4 +1,6 @@
 import {createRouter, createWebHistory} from 'vue-router'
+import store from "@/store";
+import {notification} from "ant-design-vue";
 
 const routes = [
     {
@@ -7,7 +9,10 @@ const routes = [
     },
     {
         path: '/',
-        component: () => import('../views/main.vue')
+        component: () => import('../views/main.vue'),
+        meta:{
+            loginRequire: true
+        }
     },
 ]
 
@@ -15,5 +20,25 @@ const router = createRouter({
     history: createWebHistory(process.env.BASE_URL),
     routes
 })
+
+router.beforeEach((to, from, next) => {
+    if (to.matched.some(function (item) {
+        console.log(item, "是否需要登陆校验：", item.meta.loginRequire || false);
+
+        return item.meta.loginRequire
+    })) {
+        const member = store.state.member;
+        console.log("登录页面校验开始:", member);
+        if (!member.token) {
+            console.log("用户未登录或登陆超时");
+            notification.error({description: "未登录或登陆超时"});
+            next("/login");
+        }else {
+            next();
+        }
+    }else {
+        next();
+    }
+});
 
 export default router
