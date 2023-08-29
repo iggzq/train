f71b290b2150aba2a533c74730aed5225bb49a5b
LiTz <2811328244@qq.com>
2023-08-29 10:57:24 +0800

    8.29.4 为axios增加登陆拦截，拦截后为请求头增加token

--- web/src/main.js
+++ web/src/main.js
@@ -2,7 +2,7 @@ import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import Antd from 'ant-design-vue';
+import Antd, {notification} from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
 import * as Icons from '@ant-design/icons-vue';
 import axios from 'axios';
@@ -16,20 +16,33 @@ for (const i in icons) {
     app.component(i, icons[i]);
 }
 
-axios.interceptors.request.use(function (config){
-    console.log("请求参数：",config);
+axios.interceptors.request.use(function (config) {
+    console.log("请求参数：", config);
+    const token = store.state.member.token;
+    if (token) {
+        config.headers.token = token;
+        console.log("为请求增加token header:", token);
+    }
     return config;
-},error => {
+}, error => {
     return Promise.reject(error);
 })
 
-axios.interceptors.response.use(function (response){
-    console.log("返回结果：",response);
+axios.interceptors.response.use(function (response) {
+    console.log("返回结果：", response);
     return response;
-}, error =>{
-    console.log("返回错误:",error);
+}, error => {
+    const resp = error.response;
+    const status = resp.status;
+    if (status === 401) {
+        console.log("未登录或登陆超时，跳到登陆页");
+        store.commit("setMember", {});
+        notification.error({description: "未登录或登陆超时"});
+        router.push('/login');
+    }
+    console.log("返回错误:", error);
     return Promise.reject(error);
 })
-axios.defaults.baseURL=process.env.VUE_APP_SERVER
-console.log('环境：',process.env.NODE_ENV)
-console.log('后端地址：',process.env.VUE_APP_SERVER)
+axios.defaults.baseURL = process.env.VUE_APP_SERVER
+console.log('环境：', process.env.NODE_ENV)
+console.log('后端地址：', process.env.VUE_APP_SERVER)
