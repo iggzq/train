3d09b30d58201a80cec79101616d2eb23102bee3
LiTz <2811328244@qq.com>
2023-08-29 20:21:30 +0800

    8.29.10 增加日志拦截器，统一打印日志流水号

--- member/src/main/java/com/study/train/member/config/SpringMvcConfig.java
+++ member/src/main/java/com/study/train/member/config/SpringMvcConfig.java
@@ -1,5 +1,6 @@
 package com.study.train.member.config;
 
+import com.study.train.common.interceptor.LogInterceptor;
 import com.study.train.common.interceptor.MemberInterceptor;
 import jakarta.annotation.Resource;
 import org.springframework.context.annotation.Configuration;
@@ -11,8 +12,12 @@ public class SpringMvcConfig implements WebMvcConfigurer {
     @Resource
     MemberInterceptor memberInterceptor;
 
+    @Resource
+    LogInterceptor logInterceptor;
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(logInterceptor);
         registry.addInterceptor(memberInterceptor).addPathPatterns("/**")
                 .excludePathPatterns("/member/member/send-code",
                         "/member/member/login");
