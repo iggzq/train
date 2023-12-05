43f943799aea7a8d57722e9aa59ac7d629952588
LiTz <2811328244@qq.com>
2023-12-05 20:14:07 +0800

    12.5.12 集成quartz定时任务,编写controller层代码，以便前端调用

--- batch/src/main/java/com/study/train/batch/config/QuartzConfig.java
+++ batch/src/main/java/com/study/train/batch/config/QuartzConfig.java
@@ -1,32 +1,27 @@
 package com.study.train.batch.config;
 
-import com.study.train.batch.job.TestJob;
-import org.quartz.*;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class QuartzConfig {
-
-    //声明内容
-    @Bean
-    public JobDetail jobDetail() {
-        return JobBuilder.newJob(TestJob.class)
-                .withIdentity("TestJob", "test")
-                .storeDurably()
-                .build();
-    }
-
-    //声明触发器
-    @Bean
-    public Trigger trigger() {
-
-        return TriggerBuilder
-                .newTrigger()
-                .forJob(jobDetail())
-                .withIdentity("trigger", "trigger")
-                .startNow()
-                .withSchedule(CronScheduleBuilder.cronSchedule("*/2 * * * * ?"))
-                .build();
-    }
-}
+//@Configuration
+//public class QuartzConfig {
+//
+//    //声明内容
+//    @Bean
+//    public JobDetail jobDetail() {
+//        return JobBuilder.newJob(TestJob.class)
+//                .withIdentity("TestJob", "test")
+//                .storeDurably()
+//                .build();
+//    }
+//
+//    //声明触发器
+//    @Bean
+//    public Trigger trigger() {
+//
+//        return TriggerBuilder
+//                .newTrigger()
+//                .forJob(jobDetail())
+//                .withIdentity("trigger", "trigger")
+//                .startNow()
+//                .withSchedule(CronScheduleBuilder.cronSchedule("*/2 * * * * ?"))
+//                .build();
+//    }
+//}
