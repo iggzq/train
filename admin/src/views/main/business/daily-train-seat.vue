954fe33a6fd335e4cfa0136866bcd07bb0b3c833
LiTz <2811328244@qq.com>
2023-12-14 21:06:52 +0800

    12.14.7 每日座位界面改为只读

--- admin/src/views/main/business/daily-train-seat.vue
+++ admin/src/views/main/business/daily-train-seat.vue
@@ -1,7 +1,7 @@
 <template>
     <a-space class="top_button">
             <a-button type="primary" @click="handleQuery()">刷新</a-button>
-                            <a-button type="primary" @click="onAdd">新增</a-button>
+            
     </a-space>
     <a-table :data-source="dailyTrainStationSeats"
              :columns="columns"
@@ -10,15 +10,6 @@
              :loading="loading">
         <template #bodyCell="{ column, record }">
             <template v-if="column.dataIndex === 'operation'">
-                    <a-space>
-                        <a-popconfirm
-                                title="删除后不可恢复，确认删除?"
-                                @confirm="onDelete(record)"
-                                ok-text="确认" cancel-text="取消">
-                            <a style="color: red">删除</a>
-                        </a-popconfirm>
-                        <a @click="onEdit(record)">编辑</a>
-                    </a-space>
             </template>
                     <template v-else-if="column.dataIndex === 'col'">
         <span v-for="item in SEAT_COL_ARRAY" :key="item.key">
@@ -36,39 +27,6 @@
                     </template>
         </template>
     </a-table>
-        <a-modal v-model:visible="visible" title="每日座位" @ok="handleOk" ok-text="确认" cancel-text="取消">
-            <a-form :model="dailyTrainStationSeat" :label-col="{span: 4}" :wrapper-col="{ span: 20 }">
-                        <a-form-item label="日期">
-                                    <a-date-picker v-model:value="dailyTrainStationSeat.date" valueFormat="YYYY-MM-DD"
-                                                   placeholder="请选择日期"/>
-                        </a-form-item>
-                        <a-form-item label="车次编号">
-                                <a-input v-model:value="dailyTrainStationSeat.trainCode"/>
-                        </a-form-item>
-                        <a-form-item label="箱序">
-                                <a-input v-model:value="dailyTrainStationSeat.carriageIndex"/>
-                        </a-form-item>
-                        <a-form-item label="排号">
-                                <a-input v-model:value="dailyTrainStationSeat.row"/>
-                        </a-form-item>
-                        <a-form-item label="列号">
-                                <a-select v-model:value="dailyTrainStationSeat.col">
-                                    <a-select-option v-for="item in SEAT_COL_ARRAY" :key="item.key" :value="item.key">{{item.value}}</a-select-option>
-                                </a-select>
-                        </a-form-item>
-                        <a-form-item label="座位类型">
-                                <a-select v-model:value="dailyTrainStationSeat.seatType">
-                                    <a-select-option v-for="item in SEAT_TYPE_ARRAY" :key="item.key" :value="item.key">{{item.value}}</a-select-option>
-                                </a-select>
-                        </a-form-item>
-                        <a-form-item label="同车箱座序">
-                                <a-input v-model:value="dailyTrainStationSeat.carriageSeatIndex"/>
-                        </a-form-item>
-                        <a-form-item label="售卖情况">
-                                <a-input v-model:value="dailyTrainStationSeat.sell"/>
-                        </a-form-item>
-            </a-form>
-        </a-modal>
 </template>
 
 <script>
@@ -144,52 +102,8 @@ import axios from "axios";
                     dataIndex: 'sell',
                     key: 'sell',
                 },
-                {
-                    title: '操作',
-                    dataIndex: 'operation'
-                }
             ];
 
-            const onAdd = () => {
-                dailyTrainStationSeat.value = {};
-                visible.value = true;
-            };
-
-            const onEdit = (record) => {
-                dailyTrainStationSeat.value = window.Tool.copy(record);
-                visible.value = true;
-            };
-
-            const onDelete = (record) => {
-                axios.delete("/business/admin/daily-train-seat/delete/" + record.id).then((response) => {
-                    const data = response.data;
-                    if (data.success) {
-                        notification.success({description: "删除成功！"});
-                        handleQuery({
-                            page: pagination.value.current,
-                            size: pagination.value.pageSize,
-                        });
-                    } else {
-                        notification.error({description: data.message});
-                    }
-                });
-            };
-
-            const handleOk = () => {
-                axios.post("/business/admin/daily-train-seat/save", dailyTrainStationSeat.value).then((response) => {
-                    let data = response.data;
-                    if (data.success) {
-                        notification.success({description: "保存成功！"});
-                        visible.value = false;
-                        handleQuery({
-                            page: pagination.value.current,
-                            size: pagination.value.pageSize
-                        });
-                    } else {
-                        notification.error({description: data.message});
-                    }
-                });
-            };
 
             const handleQuery = (param) => {
                 if (!param) {
@@ -245,10 +159,6 @@ import axios from "axios";
                 handleTableChange,
                 handleQuery,
                 loading,
-                onAdd,
-                handleOk,
-                onEdit,
-                onDelete
             };
         },
     });
