0d2ba7325e06cb5407e5d1c975cf1e00354ec6ea
lituizi <2811328244@qq.com>
2025-01-12 10:13:44 +0800

    refactor(business): 移除支付宝相关代码并调整配置文件 - 删除了 AliPayController 类中的支付宝支付逻辑 - 移除了 application.yml 中的支付宝配置项- 调整了 bootstrap.yaml 文件结构，简化配置 - 删除了 business、gateway 和 member 模块中不必要的依赖 - 添加了 spring-cloud-starter-bootstrap 依赖到 common 模块

--- business/src/main/java/com/study/train/business/controller/AliPayController.java
+++ business/src/main/java/com/study/train/business/controller/AliPayController.java
@@ -1,52 +1,52 @@
-package com.study.train.business.controller;
-
-import com.alipay.api.AlipayApiException;
-import com.alipay.api.AlipayClient;
-import com.alipay.api.request.AlipayTradePagePayRequest;
-import com.study.train.business.domain.ConfirmOrder;
-import com.study.train.business.req.TicketPayReq;
-import com.study.train.business.mapper.ConfirmOrderMapper;
-import jakarta.annotation.Resource;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/ticket-pay/")
-public class AliPayController {
-
-    @Resource
-    private AlipayClient alipayClient;
-    @Resource
-    private AlipayTradePagePayRequest alipayTradePagePayRequest;
-
-    @Resource
-    private ConfirmOrderMapper confirmOrderMapper;
-
-    //处理支付宝支付
-    @RequestMapping("pay")
-    public String pay(@RequestBody TicketPayReq ticketPayReq) throws AlipayApiException {
-        String tradeNum = ticketPayReq.getTradeNum();
-        Long ticketId = Long.valueOf(tradeNum);
-        ConfirmOrder confirmOrder = confirmOrderMapper.selectByPrimaryKey(ticketId);
-        System.out.println(confirmOrder);
-        String amount = String.valueOf(confirmOrder.getAmount());
-        System.out.println( "{\"out_trade_no\":\"" + ticketPayReq.getTradeNum() + "\","
-                + "\"total_amount\":\"" + amount + "\","
-                + "\"subject\":\"" + ticketPayReq.getTradeName() + "\","
-                + "\"body\":\"" + ticketPayReq.getSubject() + "\","
-                + "\"product_code\":\"FAST_INSTANT_TRADE_PAY\"}");
-
-        alipayTradePagePayRequest.setNotifyUrl("www.baidu.com");
-        alipayTradePagePayRequest.setReturnUrl("www.bilibili.com");
-        alipayTradePagePayRequest.setBizContent(
-                "{\"out_trade_no\":\"" + ticketPayReq.getTradeNum() + "\","
-                        + "\"total_amount\":\"" + amount + "\","
-                        + "\"subject\":\"" + ticketPayReq.getTradeName() + "\","
-                        + "\"body\":\"" + ticketPayReq.getSubject() + "\","
-                        + "\"product_code\":\"FAST_INSTANT_TRADE_PAY\"}");
-        return alipayClient.pageExecute(alipayTradePagePayRequest).getBody();
-
-    }
-
-}
+//package com.study.train.business.controller;
+//
+//import com.alipay.api.AlipayApiException;
+//import com.alipay.api.AlipayClient;
+//import com.alipay.api.request.AlipayTradePagePayRequest;
+//import com.study.train.business.domain.ConfirmOrder;
+//import com.study.train.business.req.TicketPayReq;
+//import com.study.train.business.mapper.ConfirmOrderMapper;
+//import jakarta.annotation.Resource;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping("/ticket-pay/")
+//public class AliPayController {
+//
+//    @Resource
+//    private AlipayClient alipayClient;
+//    @Resource
+//    private AlipayTradePagePayRequest alipayTradePagePayRequest;
+//
+//    @Resource
+//    private ConfirmOrderMapper confirmOrderMapper;
+//
+//    //处理支付宝支付
+//    @RequestMapping("pay")
+//    public String pay(@RequestBody TicketPayReq ticketPayReq) throws AlipayApiException {
+//        String tradeNum = ticketPayReq.getTradeNum();
+//        Long ticketId = Long.valueOf(tradeNum);
+//        ConfirmOrder confirmOrder = confirmOrderMapper.selectByPrimaryKey(ticketId);
+//        System.out.println(confirmOrder);
+//        String amount = String.valueOf(confirmOrder.getAmount());
+//        System.out.println( "{\"out_trade_no\":\"" + ticketPayReq.getTradeNum() + "\","
+//                + "\"total_amount\":\"" + amount + "\","
+//                + "\"subject\":\"" + ticketPayReq.getTradeName() + "\","
+//                + "\"body\":\"" + ticketPayReq.getSubject() + "\","
+//                + "\"product_code\":\"FAST_INSTANT_TRADE_PAY\"}");
+//
+//        alipayTradePagePayRequest.setNotifyUrl("www.baidu.com");
+//        alipayTradePagePayRequest.setReturnUrl("www.bilibili.com");
+//        alipayTradePagePayRequest.setBizContent(
+//                "{\"out_trade_no\":\"" + ticketPayReq.getTradeNum() + "\","
+//                        + "\"total_amount\":\"" + amount + "\","
+//                        + "\"subject\":\"" + ticketPayReq.getTradeName() + "\","
+//                        + "\"body\":\"" + ticketPayReq.getSubject() + "\","
+//                        + "\"product_code\":\"FAST_INSTANT_TRADE_PAY\"}");
+//        return alipayClient.pageExecute(alipayTradePagePayRequest).getBody();
+//
+//    }
+//
+//}
